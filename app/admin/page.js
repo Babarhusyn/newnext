3f9f6e371a52298143b8f101eada862b965e0dfc
Babarhusyn <babarofficial32@gmail.com>
2024-01-21 09:07:31 +0500

    jkl

---
+++ app/admin/page.js
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+import Add from '@/app/admin/components/add'
+const page = () => {
+    return (
+        <div>
+            <Add />
+            Hello
+        </div>
+    )
+}
+
+export default page
