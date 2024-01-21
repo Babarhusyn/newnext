f0637d1f5ce62b2205ade001bb87cfa9d02ffd32
Babarhusyn <babarofficial32@gmail.com>
2024-01-21 09:11:33 +0500

    new comit

---
+++ app/users/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Update from '@/app/users/components/update'
+
+const page = () => {
+    return (
+        <div>
+            <Update />
+        </div>
+    )
+}
+
+export default page
